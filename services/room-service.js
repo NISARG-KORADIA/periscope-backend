c149c36138dd857659d27e5ed613cbed0879bc2e
NISARG-KORADIA <mknisarg@gmail.com>
2022-11-20 20:55:45 +0530

    Rooms and users api are updated

---
+++ services/room-service.js
@@ -0,0 +1,39 @@
+const RoomModel = require('../models/room-model.js');
+
+class RoomService {
+  async create(payload) {
+    const { topic, roomType, tags, hostId } = payload;
+    const room = await RoomModel.create({
+      topic,
+      roomType,
+      tags,
+      hostId,
+      speakers: [hostId],
+    });
+    return room;
+  }
+
+  // to-do: add audience field later.
+  async getAllRooms(types) {
+    const rooms = await RoomModel.find({ roomType: { $in: types } })
+      .populate('speakers', 'name userName avatar')
+      .populate('hostId', 'name userName avatar')
+      .exec();
+    return rooms;
+  }
+
+  async getRoom(roomId) {
+    const room = await RoomModel.findOne({ _id: roomId })
+      .populate('speakers', 'name userName avatar')
+      .populate('hostId', 'name userName avatar')
+      .exec();
+    return room;
+  }
+
+  async removeRoom(roomId) {
+    await RoomModel.deleteOne({ _id: roomId })
+  }
+
+}
+
+module.exports = new RoomService();
